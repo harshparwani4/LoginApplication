f64005df3daea20bcd075e1e547a4f27dc0a1b47
harshparwani4 <harsh.parwani@tracxn.com>
2019-07-29 02:31:52 +0530

    Server Added

--- server.js
+++ server.js
@@ -19,19 +19,7 @@ app.use(
     })
 )
 app.use(express.static(path.join(__dirname, 'frontendBundle')));
-const directoryPath = path.join(__dirname, './');
 const directoryPath1 = path.join(__dirname, 'frontendBundle');
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file);
-    });
-});
 fs.readdir(directoryPath1, function (err, files) {
     //handling error
     if (err) {
